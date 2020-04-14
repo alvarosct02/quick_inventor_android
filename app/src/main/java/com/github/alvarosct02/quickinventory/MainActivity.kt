c95117e5acb3c371df86570eb4a9c7d3aeefa2bf
Alvaro Santa Cruz <alvaro.santacruz@pucp.pe>
2020-04-14 16:39:18 -0500

    init

---
+++ app/src/main/java/com/github/alvarosct02/quickinventory/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.alvarosct02.quickinventory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
